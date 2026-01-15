bc2d7d39055c2ce0742e32579ec6bf4b51198732
Suhagshetty <suhagshetty07@gmail.com>
2026-01-15 12:42:45 +0530

    Basic express

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,25 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("This is the home route");
 });
+
 app.get("/api/notes", (req, res) => {
-  res.send("Welcome to Notes route");
+  res.send("This is the notes route");
 });
-app.get("/api/suhag", (req, res) => {
-  res.status(200).send("This is the owners route");
+
+app.post("/api/suhag", (req, res) => {
+  res
+    .status(201)
+    .json({ message: "Notes created using POST methods successfully" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Notes updated using PUT methods" });
+});
+
+app.delete("api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Notes deleted successfully" });
 });
 
 app.listen(3000, () => {
-  console.log("App is running on port 3000");
+  console.log("Server is running on port 3000");
 });
